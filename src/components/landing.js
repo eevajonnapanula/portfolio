72d45ba5fc933c72acedc0de7e2f34a108c47542
Eeva-Jonna <eevis@kidescience.com>
2018-08-10 14:10:44 +0300

    Remove animation from landing-component

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,36 +1,30 @@
 import React from 'react'
 import Hero from 'grommet/components/Hero'
 import Section from 'grommet/components/Section'
-import Animate from 'grommet/components/Animate'
 import Box from 'grommet/components/Box'
 import Card from 'grommet/components/Card'
 import Anchor from 'grommet/components/Anchor'
+
 const Landing = () => (
-  <Animate
-    enter={{"animation": "slide-right", "duration": 2000, "delay": 300}}
-    keep={true}
-    leave={{"animation": "fade", "duration": 0, "delay": 0}}
-    visible="scroll">
-    <Section className="content-container" id="landing" pad="small">
-      <Hero>
-        <Box direction='row'
-          justify='center'
-          align='center'>
-          <Box
-            align='start'
-            pad='medium'>
-            <Box colorIndex='brand'>
-              <Card heading='Moi!'
-                description='Here will be something!'
-                link={<Anchor href='#about'
-                primary={true}
-                label='Go to next section' />} />
-            </Box>
+  <Section className="content-container" id="landing" pad="small">
+    <Hero>
+      <Box direction='row'
+        justify='center'
+        align='center'>
+        <Box
+          align='start'
+          pad='medium'>
+          <Box colorIndex='brand'>
+            <Card heading='Moi!'
+              description='Here will be something!'
+              link={<Anchor href='#about'
+              primary={true}
+              label='Go to next section' />} />
           </Box>
         </Box>
-      </Hero>
-    </Section>
-  </Animate>
+      </Box>
+    </Hero>
+  </Section>
 )
 
 export default Landing
